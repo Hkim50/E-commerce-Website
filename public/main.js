2f733655e9da4e671a93f10818d5c02ce4d8ac2e
williamyuuu <williamyuuu@gmail.com>
2023-11-27 20:39:23 -0800

    added js for banner slideshow

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 let storedEmail = "";
 let storedPassword = "";
+let slideIndex = 0;
+showSlides();
 
 function signup() {
   const email = document.getElementById("signupEmail").value;
@@ -33,4 +35,26 @@ function login() {
 
 function cart() {
     window.location.href = "/cart.html";
+}
+
+function showSlides() {
+  const seconds = 8;
+
+  let i;
+  let timer = seconds * 1000;
+  let slides = document.getElementsByClassName("banner-slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, timer);
 }
